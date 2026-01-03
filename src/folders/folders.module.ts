de7eaf4613e16d6a7f7e8eacc940a9a79d26e6cc
PR0T0TYP3-007 <pr8787yp3.007@gmail.com>
2026-01-03 21:02:57 +0100

    Add S3StorageService to FoldersModule providers

--- src/folders/folders.module.ts
+++ src/folders/folders.module.ts
@@ -7,11 +7,12 @@ import { SharingModule } from 'src/sharing/sharing.module';
 import { File } from 'src/database/file.entity';
 import { FileVersion } from 'src/database/file-version.entity';
 import { Users } from 'src/database/user.entity';
+import { S3StorageService } from 'src/common/s3-storage.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Folder, File, FileVersion, Users]), SharingModule],
   controllers: [FoldersController],
-  providers: [FolderService],
+  providers: [FolderService, S3StorageService],
   exports: [FolderService],
 })
 export class FoldersModule {}
